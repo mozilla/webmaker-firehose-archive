e036580256ac4332adf603f0e6cbb4275158bf3b
Ross Bruniges <rossbruniges@gmail.com>
2013-04-08 15:16:12 -0400

    Fixing up nits and annoyances. Better CSS, asset compression, less spaghetti code. Moar awesome

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
-var express = require('express'),
-    path = require('path');
+var express = require( "express" ),
+    path = require( "path" );
 
 var app = express();
 
-app.use(express.logger('dev'));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use( express.logger( "dev" ) );
+app.use( express.static( path.join( __dirname, "public" ) ) );
+app.use( express.compress() );
 
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
+app.set( "views", __dirname + "/views" );
+app.set( "view engine", "jade" );
 
-app.get('/', function(req, res) {
-    res.render('index', {});
-});
+app.get( "/", function( req, res ) {
+    res.render( "index", {} );
+} );
 
-app.listen(process.env.PORT || 3000);
+app.listen( process.env.PORT || 3000 );
--- public/assets/js/firehose.js
+++ public/assets/js/firehose.js
@@ -1,27 +1,25 @@
-/* TODO:
-  - add in a form for finding remixes on a special projectID
-  - columns, add in a column grid to make things look less shit
-  - css, make some better CSS and remove the need for the butter.ui.css file?
-  - chuck in the ability to search for more than 20 things at a time
-  - lots of repeated code, we should fix that I think
-  - convert templates to nunjucks
-
-  ANNOYS:
-  - fix broken image (this is in webmaker nav)
+/* ANNOYS:
+  - fix broken image (this is in webmaker nav so need to fix there)
+  - chuck in the ability to search for more than 20 things at a time?
+  - convert templates to nunjucks?
 */
 
 var firehose = window.firehouse || {};
 
 firehose = function() {
 
-  var insertAfter, renderResponse, ignition, init;
+  var insertAfter, renderResponse, ignition, remixSearch, init;
 
   insertAfter = function( referenceNode, newNode ) {
     referenceNode.parentNode.insertBefore( newNode, referenceNode.nextSibling );
   };
 
-  renderResponse = function( data, sibling, parent ) {
-    var frag = document.createDocumentFragment();
+  renderResponse = function( data, sibling, list_class ) {
+    var parent = document.createElement( "ol" ),
+        frag = document.createDocumentFragment();
+    if ( list_class ) {
+      parent.className = list_class;
+    }
     if ( !data.length ) {
       var li = document.createElement( "li" );
       li.innerHTML = "No remixes";
@@ -30,45 +28,59 @@ firehose = function() {
       data.forEach( function( d ) {
         var li = document.createElement( "li" ),
             viewUrl = "<a href='" + d.publishUrl + "'>" + d.name + "</a>",
-            remixUrl = "<a data-remixId='" + d.id + "' class='remix' href='remixes/" + d.id + "'>show me the remixes</a>";
-        li.innerHTML = viewUrl + " - " + remixUrl;
+            remixUrl = "<a data-remixId='" + d.id + "' class='remix butter-btn btn-green start' href='remixes/" + d.id + "'>Find remixes</a>";
+        li.innerHTML = viewUrl + " "  + remixUrl;
+        li.className = "make";
         frag.appendChild( li );
       } );
       parent.appendChild( frag );
     }
     insertAfter( sibling, parent );
+    parent.addEventListener( "click", ignition );
   };
 
   ignition = function( e ) {
     var target = e.target;
-    if ( target.className === "remix" ) {
-      var remixId = target.getAttribute( "data-remixId");
+    if ( target.hasAttribute( "data-remixId" ) ) {
+      var remixId = target.getAttribute( "data-remixId" );
       $.ajax({
         type: "GET",
         dataType: "json",
         url: "https://popcorn.webmaker.org/api/project/" + remixId + "/remixes",
         success : function( response ) {
-          renderResponse ( response.results, target, document.createElement( "ol" ) );
-        target.parentNode.removeChild( target );
+          renderResponse ( response.results, target, "remixes" );
+          target.parentNode.removeChild( target );
         }
       });
       e.preventDefault();
     }
   };
 
+  remixSearch = function( e ) {
+    var remixID = document.getElementById( "want_remix" ).value,
+        sibling = e.target.nextSibling;
+    if ( sibling ) {
+      sibling.parentNode.removeChild( sibling );
+    }
+    $.ajax({
+        type: "GET",
+        dataType: "json",
+        url: "https://popcorn.webmaker.org/api/project/" + remixID + "/remixes",
+        success : function( response ) {
+          renderResponse ( response.results, e.target, "remixes" );
+        }
+      });
+    e.preventDefault();
+  };
+
   init = function() {
-    var create = document.getElementById( "new" ),
-        apiCreates = document.createElement( "ol" ),
-        updates = document.getElementById( "updated" ),
-        apiUpdates = document.createElement( "ol" ),
-        remixed = document.getElementById( "updated" ),
-        remixedUpdates = document.createElement( "ol" );
+    var remixForm = document.getElementById( "search_remixes" );
     $.ajax({
         type: "GET",
         dataType: "json",
         url: "https://popcorn.webmaker.org/api/projects/recentlyCreated/20",
         success : function( response ) {
-          renderResponse( response.results, create, apiCreates );
+          renderResponse( response.results, document.getElementById( "new" ) );
         }
       });
     $.ajax({
@@ -76,7 +88,7 @@ firehose = function() {
         dataType: "json",
         url: "https://popcorn.webmaker.org/api/projects/recentlyUpdated/20",
         success : function( response ) {
-          renderResponse( response.results, updates, apiUpdates  );
+          renderResponse( response.results, document.getElementById( "updated" ) );
         }
       });
     $.ajax({
@@ -84,12 +96,10 @@ firehose = function() {
         dataType: "json",
         url: "https://popcorn.webmaker.org/api/projects/recentlyRemixed/20",
         success : function( response ) {
-          renderResponse( response.results, remixed, remixedUpdates  );
+          renderResponse( response.results, remixed = document.getElementById( "updated" ) );
         }
       });
-    apiCreates.addEventListener( "click", ignition );
-    apiUpdates.addEventListener( "click", ignition );
-    remixedUpdates.addEventListener( "click", ignition );
+    remixForm.addEventListener( "submit", remixSearch );
   };
 
   return {
