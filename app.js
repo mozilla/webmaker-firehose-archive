7f465fbce795c2c383be8acd140dec2fdfe3db63
Ross Bruniges <rossbruniges@gmail.com>
2013-04-08 10:00:20 -0400

    A bit more scaffolding and templates

--- app.js
+++ app.js
@@ -6,4 +6,11 @@ var app = express();
 app.use(express.logger('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+
+app.get('/', function(req, res) {
+    res.render('index', {});
+});
+
 app.listen(process.env.PORT || 3000);
